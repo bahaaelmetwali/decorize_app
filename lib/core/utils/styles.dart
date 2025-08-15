8ec7bcddf5c804bad7ad5ab82d64a6252c5ddad1
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-08-15 04:57:07 +0300

    Class_styles_forTextadded

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+static final textStyle28 = GoogleFonts.ibmPlexSansArabic(
+  fontSize: 28,
+  fontWeight: FontWeight.w600,
+);
+  static final textStyle20 = GoogleFonts.ibmPlexSansArabic(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static final textStyle30 = GoogleFonts.ibmPlexSansArabic(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+  static final textStyle14 = GoogleFonts.ibmPlexSansArabic(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static final textStyle16 = GoogleFonts.ibmPlexSansArabic(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+}
